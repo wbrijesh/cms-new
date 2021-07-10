2a4ec9114eb346906379fab1ba1ba32e51be8fa1
wbrijesh <brijesh@wawdhane.com>
2021-07-10 10:55:00 +0530

    fix authType not authorised to access any resource

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import "../styles/globals.css";
 import { withAuthenticator } from "@aws-amplify/ui-react";
-import Amplify from "aws-amplify";
+import Amplify, { AuthModeStrategyType } from "aws-amplify";
 import config from "../aws-exports";
 Amplify.configure({
   ...config,
+  DataStore: {
+    authModeStrategyType: AuthModeStrategyType.MULTI_AUTH,
+  },
   ssr: true,
 });
 
