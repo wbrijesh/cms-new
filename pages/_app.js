68ae7c1c2d5152c07b4bf0b387dee8ebff352ac8
wbrijesh <brijesh@wawdhane.com>
2021-07-14 11:39:58 +0530

    add sentry

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,24 @@ import "../styles/globals.css";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import Amplify, { AuthModeStrategyType } from "aws-amplify";
 import config from "../aws-exports";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 Amplify.configure({
   ...config,
   DataStore: {
     authModeStrategyType: AuthModeStrategyType.MULTI_AUTH,
   },
   ssr: true,
+});
+
+Sentry.init({
+  dsn: "https://b71fbb7f1fdb47378c354b0ccbdba8af@o919596.ingest.sentry.io/5863840",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
 });
 
 function MyApp({ Component, pageProps }) {
